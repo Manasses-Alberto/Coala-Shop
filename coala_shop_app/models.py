d078cac8f30277dfc94bab332e3e63c7015f91ff
Manasses-Alberto <manassesndombele88@gmail.com>
2023-12-23 17:09:12 +0100

    Fiz as minhas models para registrar Produtos, Compras e Likes em produtos e fiz outras configurações de mídia

--- coala_shop_app/models.py
+++ coala_shop_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=80)
+    price = models.PositiveBigIntegerField()
+    description = models.TextField(max_length=300)
+    iva = models.IntegerField()
+    image = models.ImageField(upload_to='')
+    added = models.DateTimeField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'coala_shop_app_products'
+
+class Shoppings(models.Model):
+    id = models.AutoField(primary_key=True)
+    buyer = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+
+class Products_Likes(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
