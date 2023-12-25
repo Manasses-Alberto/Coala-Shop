48dadb04c43e2b40428b9896fc7325aa48a1f701
Manasses-Alberto <manassesndombele88@gmail.com>
2023-12-25 15:52:51 +0100

    Fiz os estilos do modal de accões de conta do usuário apenas na versão desktop

--- coala_shop_app/static/scripts/index.js
+++ coala_shop_app/static/scripts/index.js
@@ -1,4 +1,28 @@
-for (let card of document.querySelectorAll('.card')) {
+document.querySelector('#account-actions-modal-btn').addEventListener('click', () => {
+    document.querySelector('header').style.opacity = '0.1'
+    document.querySelector('header').style.pointerEvents = 'none'
+    document.querySelector('article').style.opacity = '0.1'
+    document.querySelector('article').style.pointerEvents = 'none'
+    document.querySelector('footer').style.opacity = '0.1'
+    document.querySelector('footer').style.pointerEvents = 'none'
+    document.body.style.overflowY = 'hidden'
+    document.querySelector('#modal-account-actions').classList.remove('visually-hidden')
+    document.querySelector('#modal-account-actions').style.opacity = '1'
+})
+
+document.querySelector('#account-actions-modal-xbtn').addEventListener('click', () => {
+    document.querySelector('header').style.opacity = '1'
+    document.querySelector('header').style.pointerEvents = 'auto'
+    document.querySelector('article').style.opacity = '1'
+    document.querySelector('article').style.pointerEvents = 'auto'
+    document.querySelector('footer').style.opacity = '1'
+    document.querySelector('footer').style.pointerEvents = 'auto'
+    document.body.style.overflowY = 'auto'
+    document.querySelector('#modal-account-actions').classList.add('visually-hidden')
+    document.querySelector('#modal-account-actions').style.opacity = '0'
+})
+
+for (let card of document.querySelector('article').querySelectorAll('.card')) {
     console.log(card.innerHTML)
     card.addEventListener('mouseover', () => {
         card.style.transform = 'scale(1.03)'
