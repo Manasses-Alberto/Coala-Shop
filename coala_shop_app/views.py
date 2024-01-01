c4648c63a0efd8907f278b5b993b91d3cc22b2d9
Manasses-Alberto <manassesndombele88@gmail.com>
2024-01-01 21:32:54 +0100

    Modifiquei as rotas das views login e register

--- coala_shop_app/views.py
+++ coala_shop_app/views.py
@@ -6,12 +6,11 @@ from coala_shop_app.models import Product, Shoppings
 from datetime import datetime
 
 def home(request):
-    print(f'{request.user.is_authenticated}')
-    return render(request, 'index.html', {'products': Product.objects.all(), 'login': False, 'register': False})
+    return render(request, 'index.html', {'products': Product.objects.all()})
 
 def login(request):
     if request.method == 'GET':
-        return render(request, 'index.html', {'products': Product.objects.all(), 'login': True, 'register': False})
+        return render(request, 'login.html', {'products': Product.objects.all(), 'login': True, 'register': False})
 
     elif request.method == 'POST':
         email = request.POST['email'].strip().lower()
@@ -20,7 +19,7 @@ def login(request):
             username = User.objects.get(email=email)
 
         except User.DoesNotExist:
-            return render(request, 'index.html', {'login': True, 'error': True, 'field_error': ['email'], 'message': 'O seu email está inválido. Tente novamente'})
+            return render(request, 'login.html', {'login': True, 'error': True, 'field_error': ['email'], 'message': 'O seu email está inválido. Tente novamente'})
 
         else:
             user = authenticate(request, username=username, password=password)
@@ -28,7 +27,7 @@ def login(request):
                 auth_login(request, user)
             
             except AttributeError:
-                return render(request, 'index.html', {'login': True, 'error': True, 'field_error': ['password'], 'message': 'A sua senha está inválida. Tente novamente'})
+                return render(request, 'login.html', {'login': True, 'error': True, 'field_error': ['password'], 'message': 'A sua senha está inválida. Tente novamente'})
 
             else:
                 return redirect('home')
@@ -40,7 +39,7 @@ def logout(request):
 
 def register(request):
     if request.method == 'GET':
-        return render(request, 'index.html', {'register': True})
+        return render(request, 'register.html', {'register': True})
 
     elif request.method == 'POST':
         first_name = request.POST['first-name']
@@ -51,19 +50,19 @@ def register(request):
         old_username = User.objects.filter(username=f'{first_name.strip().title()} {surname.strip().title()}').first()
         old_email = User.objects.filter(email=email.strip().lower()).first()
         if not first_name.isalpha():
-            return render(request, 'index.html', {'error': True, 'field_error': ['first_name'], 'message': 'O primeiro nome é inválido não inclua símbolos, números ou espaços...', 'register': True})
+            return render(request, 'register.html', {'error': True, 'field_error': ['first_name'], 'message': 'O primeiro nome é inválido não inclua símbolos, números ou espaços...', 'register': True})
 
         elif not surname.isalpha():
-            return render(request, 'index.html', {'error': True, 'field_error': ['surname'], 'message': 'O sobrenome é inválido não inclua símbolos, números ou espaços...', 'register': True})
+            return render(request, 'register.html', {'error': True, 'field_error': ['surname'], 'message': 'O sobrenome é inválido não inclua símbolos, números ou espaços...', 'register': True})
 
         elif old_username:
-            return render(request, 'index.html', {'error': True, 'field_error': ['first_name', 'surname'], 'message': 'O sistema já encontrou outro usuário com esses nomes...', 'register': True})
+            return render(request, 'register.html', {'error': True, 'field_error': ['first_name', 'surname'], 'message': 'O sistema já encontrou outro usuário com esses nomes...', 'register': True})
 
         elif old_email:
-            return render(request, 'index.html', {'error': True, 'field_error': ['email'], 'message': 'O sistema já encontrou outro usuário com esse email...', 'register': True})
+            return render(request, 'register.html', {'error': True, 'field_error': ['email'], 'message': 'O sistema já encontrou outro usuário com esse email...', 'register': True})
 
         elif password != password_conf:
-            return render(request, 'index.html', {'error': True, 'field_error': ['password', 'password-confirmation'], 'message': 'Verifique as senhas estão diferentes...', 'register': True})
+            return render(request, 'register.html', {'error': True, 'field_error': ['password', 'password-confirmation'], 'message': 'Verifique as senhas estão diferentes...', 'register': True})
 
         else:
             new_user = User.objects.create_user(first_name=first_name.strip().title(), last_name=surname.strip().title(), username=f'{first_name.strip().title()} {surname.strip().title()}', email=email.strip().lower(), password=password)
