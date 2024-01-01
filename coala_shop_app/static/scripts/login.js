6f06902be89bab92b23c6bcc13b88ce5f38d6416
Manasses-Alberto <manassesndombele88@gmail.com>
2024-01-01 21:20:17 +0100

    Removi os modais de login e registro para templates individuais

---
+++ coala_shop_app/static/scripts/login.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    document.body.style.overflowY = 'hidden'
+    document.querySelector('#modal-login').style.opacity = '1'
+}
---
+++ coala_shop_app/static/scripts/register.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    document.body.style.overflowY = 'hidden'
+    document.querySelector('#modal-register').style.opacity = '1'
+}
