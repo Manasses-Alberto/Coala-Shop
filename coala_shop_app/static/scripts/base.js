ebcd16c9e085165088f9107add30c001e37f8853
Manasses-Alberto <manassesndombele88@gmail.com>
2024-01-02 14:43:41 +0100

    Resolvi algumas falhas de estilo nas páginas de login, registro e na homepage

--- coala_shop_app/static/scripts/base.js
+++ coala_shop_app/static/scripts/base.js
@@ -1,18 +1,16 @@
-document.querySelector('#account-actions-modal-btn').addEventListener('click', () => {
-    if (location.pathname == '/') {
-        if (document.querySelector('span#user-session-status').textContent === 'FALSE') {
-            document.querySelector('header').style.opacity = '0'
-            document.querySelector('header').style.pointerEvents = 'none'
-            document.querySelector('article').style.opacity = '0'
-            document.querySelector('article').style.pointerEvents = 'none'
-            document.querySelector('footer').style.opacity = '0'
-            document.querySelector('footer').style.pointerEvents = 'none'
-            document.body.style.overflowY = 'hidden'
-            document.querySelector('#modal-account-actions').classList.remove('visually-hidden')
-            document.querySelector('#modal-account-actions').style.opacity = '1'
-        } else {
-            location.href = `/user-profile/${document.querySelector('span#user-id').textContent}/`
-        }
+document.querySelector('button#account-actions-modal-btn').addEventListener('click', () => {
+    if (document.querySelector('article#products-datas').getAttribute('data-authenticated') === 'False') {
+        document.querySelector('header').style.opacity = '0'
+        document.querySelector('header').style.pointerEvents = 'none'
+        document.querySelector('article').style.opacity = '0'
+        document.querySelector('article').style.pointerEvents = 'none'
+        document.querySelector('footer').style.opacity = '0'
+        document.querySelector('footer').style.pointerEvents = 'none'
+        document.body.style.overflowY = 'hidden'
+        document.querySelector('#modal-account-actions').classList.remove('visually-hidden')
+        document.querySelector('#modal-account-actions').style.opacity = '1'
+    } else {
+        location.href = `/user-profile/${document.querySelector('article#products-datas').getAttribute('data-user')}/`
     }
 })
 
--- coala_shop_app/static/scripts/index.js
+++ coala_shop_app/static/scripts/index.js
@@ -1,14 +1,40 @@
-for (let card of document.querySelector('article').querySelectorAll('.card')) {
-    card.addEventListener('mouseover', () => {
+function card_hover_effect(over, leave) {
+    if (over) {
         card.style.transform = 'scale(1.03)'
         card.querySelector('.card-title').style.color = '#B4BEC9'
         card.querySelector('p').style.color = '#B4BEC9'
-    })
+    }
 
-    card.addEventListener('mouseleave', () => {
+    if (leave) {
         card.style.transform = 'scale(1)'
         card.querySelector('.card-title').style.color = '#002333'
         card.querySelector('p').style.color = '#002333'
+    }
+}
+
+for (let card of document.querySelector('#products-datas').querySelectorAll('.card')) {
+    card.querySelector('.card-title').addEventListener('mouseover', () => {
+        card_hover_effect(over=true, leave=false)
+    })
+
+    card.querySelector('.card-img-top').addEventListener('mouseover', () => {
+        card_hover_effect(over=true, leave=false)
+    })
+
+    card.querySelector('p').addEventListener('mouseover', () => {
+        card_hover_effect(over=true, leave=false)
+    })
+
+    card.querySelector('.card-title').addEventListener('mouseleave', () => {
+        card_hover_effect(over=false, leave=true)
+    })
+
+    card.querySelector('.card-img-top').addEventListener('mouseleave', () => {
+        card_hover_effect(over=false, leave=true)
+    })
+
+    card.querySelector('p').addEventListener('mouseleave', () => {
+        card_hover_effect(over=false, leave=true)
     })
 
     card.addEventListener('click', () => {
@@ -25,8 +51,8 @@ document.querySelector('#account-actions-modal-xbtn').addEventListener('click', 
     document.querySelector('footer').style.opacity = '1'
     document.querySelector('footer').style.pointerEvents = 'auto'
     document.body.style.overflowY = 'auto'
-    document.querySelector('#modal-account-actions').classList.add('visually-hidden')
     document.querySelector('#modal-account-actions').style.opacity = '0'
+    document.querySelector('#modal-account-actions').classList.add('visually-hidden')
 })
 
 document.querySelector('#login-btn').addEventListener('click', () => {
@@ -35,8 +61,4 @@ document.querySelector('#login-btn').addEventListener('click', () => {
 
 document.querySelector('#register-btn').addEventListener('click', () => {
     location.href = '/auth/register/'
-})
-
-document.querySelector('button#logout-btn').addEventListener('click', () => {
-    location.href = '/auth/logout/'
 })
